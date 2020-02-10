b8453ef6deeddc725e912f0716c89f1b6c518f80
Badr Choubai <badrchoubai@outlook.com>
2020-02-10 15:11:56 -0700

    (code-review-2): per last code review refactor PP2(p2)

--- PythonProject1/BadrChoubai_S_01__02.py
+++ PythonProject1/BadrChoubai_S_01__02.py
@@ -28,11 +28,11 @@ The Problem:
 
 
 def get_user_input(prompts: list) -> list:
-    prompt_answers = [None]*len(prompts)
+    prompt_answers: list = []
 
-    for i, prompt_message in enumerate(prompts):
-        answer = input(str(f"{ prompt_message }: "))
-        prompt_answers[i] = int(answer.strip())
+    for prompt_message in prompts:
+        answer = input(f"{ prompt_message }: ")
+        prompt_answers.append(int(answer.strip()))
 
     return prompt_answers
 
@@ -41,7 +41,7 @@ def main():
     prompts: list = ["Enter miles travelled",
                      "Gallons of fuel used? "]
 
-    [miles, gallons] = get_user_input(prompts) 
+    miles, gallons = get_user_input(prompts) 
 
     kilometers = (miles * 1.60934)
     liters = (gallons * 3.7854)
@@ -53,15 +53,13 @@ def main():
     liters_per_kilometer = 1 / \
         (miles_per_gallon * kilometers_per_mile * gallons_per_liter)
 
-    mpg_str = str(f"{miles_per_gallon:.2f} mpg")
-    lpk_str = str(f"{liters_per_kilometer:.2f} lpk")
+    mpg_str = f"{miles_per_gallon:.2f} mpg"
+    lpk_str = f"{liters_per_kilometer:.2f} lpk"
 
-    output_str = str(
-        f"Miles Per Gallon: {mpg_str} \nLiters Per Kilometer: {lpk_str}")
+    output_str = f"Miles Per Gallon: {mpg_str} \nLiters Per Kilometer: {lpk_str}"
 
-    return output_str
+    print(output_str)
 
 
 if __name__ == "__main__":
-    result = main()
-    print(result)
+    main()
