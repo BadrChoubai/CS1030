da75dede8fc3a77b1f48ed91ff1bf1d3db577019
Badr Choubai <badrchoubai@outlook.com>
2020-02-20 10:50:11 -0700

    (Python Project 2): P2, check off requirements done.

--- Python_Projects/python_project_2/badr_choubai_03_02__02.py
+++ Python_Projects/python_project_2/badr_choubai_03_02__02.py
@@ -1,13 +1,13 @@
 """
-    2.1 Ask the user how many simulations they want to run, say N. If the user presses the <Enter> key or enters 0 or a negative number, exit the program.
+    # 2.1 Ask the user how many simulations they want to run, say N. If the user presses the <Enter> key or enters 0 or a negative number, exit the program.
 
-    2.2 Loop N times.
+    # 2.2 Loop N times.
 
-    2.3 Loop until you find three heads or three tails in a row, HHH or TTT, respectively.
+    # 2.3 Loop until you find three heads or three tails in a row, HHH or TTT, respectively.
 
-    2.4 Start flipping coins. Use randint(1,2) to generate a 1 or 2 for heads and tails respectively.
+    # 2.4 Start flipping coins. Use randint(1,2) to generate a 1 or 2 for heads and tails respectively.
 
-    2.5 Count flips and check for HHH or TTT.
+    # 2.5 Count flips and check for HHH or TTT.
 
     2.6 When you get HHH or TTT, print the flips in the current simulation and accumulate the number of flips, minimum number of flips to get three in a row and the highest number of flips to get three in a row. The flips should be displayed with Hs and Ts with a space between each flip, like H T T H H H
 
