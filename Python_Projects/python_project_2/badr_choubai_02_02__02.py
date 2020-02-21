0b18515aa76753af831396779cdabefc3a872f18
Badr Choubai <badrchoubai@outlook.com>
2020-02-21 10:48:50 -0700

    (PP2): Problem Two, 2.1 and 2.2

--- Python_Projects/python_project_2/badr_choubai_02_02__02.py
+++ Python_Projects/python_project_2/badr_choubai_02_02__02.py
@@ -3,15 +3,15 @@
 
     # 2.2 Loop N times.
 
-    # 2.3 Loop until you find three heads or three tails in a row, HHH or TTT, respectively.
+    2.3 Loop until you find three heads or three tails in a row, HHH or TTT, respectively.
 
-    # 2.4 Start flipping coins. Use randint(1,2) to generate a 1 or 2 for heads and tails respectively.
+    2.4 Start flipping coins. Use randint(1,2) to generate a 1 or 2 for heads and tails respectively.
 
-    # 2.5 Count flips and check for HHH or TTT.
+    2.5 Count flips and check for HHH or TTT.
 
     2.6 When you get HHH or TTT, print the flips in the current simulation and accumulate the number of flips, minimum number of flips to get three in a row and the highest number of flips to get three in a row. The flips should be displayed with Hs and Ts with a space between each flip, like H T T H H H
         
-        choice = 'H' or 'T'    
+        choice = choice(('H', 'T'))    
         Something like ''.join(f'{ choice } ') 
 
     2.7 Go on to the next simulation, if there is one.
@@ -22,51 +22,22 @@
 """
 from random import choice
 
-def calculate_series_results(series: dict) -> dict:
-    pass
 
 def is_valid_series(series: list) -> bool:
-    series = ''.join(series)
-
-    valid = ('TTT' in series or 'HHH' in series)
+    triplet = ''.join(series)
+    valid = ('TTT' in triplet or 'HHH' in triplet)
     return valid
 
 
 def main():
-    simulations = input("How many simulations would you like to run?: ")
+    simulations = input(
+        "How many coin flipping simulations would you like to run?: ")
     simulations = int(simulations)
-    print(f"\nSimulations to run: { simulations } ")
-    coin = ('H', 'T')
-    series = {
-        'avg_flips': 0,
-        'flips_in_series': 0,
-        'max_flips': 0,
-        'min_flips': 0,
-        'series': [],
-        'series_str': "",
-    }
 
     while simulations > 0:
-        print("while: simulations > 0\n")
-        while len(series["series"]) < 3:
-            print("while: len(series) < 3")
-            coin_flip = choice(coin)
-            series["flips_in_series"] += 1
-            series["series"].append(coin_flip)
-        
-        print(series)
-        simulations -= 1
+        print(simulations)
 
-        if is_valid_series(series['series']):
-            print("if: is_valid_series?")
-            print(f"Flips: { series['flips_in_series'] }")
-            print(f"Simulations left: { simulations }")
-            series["series"].clear()
-            break
-        else:
-            print("else")
-            series["series"].clear()
-            continue
+        simulations -= 1
 
 
 if __name__ == "__main__":
