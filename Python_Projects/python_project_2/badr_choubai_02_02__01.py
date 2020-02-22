b2710d39b0816967633f0000aaa72edafdfa90c7
Badr Choubai <badrchoubai@outlook.com>
2020-02-22 13:21:08 -0700

    (Python Project 2): problem 1: update to requirements and stubbed out functions

--- Python_Projects/python_project_2/badr_choubai_02_02__01.py
+++ Python_Projects/python_project_2/badr_choubai_02_02__01.py
@@ -1,9 +1,9 @@
 """
-    1.1 Loop until the user enters ‘quit’ at which time proceed at step 1.6.
+    # 1.1 Loop until the user enters ‘quit’ at which time proceed at step 1.6.
+ 
+    # 1.2 Do an inner loop until the user enters a blank line.
 
-    1.2 Do an inner loop until the user enters a blank line.
-
-    1.3 Prompt the user for the next grade. If the grade is invalid (e.g., E+, +A, G, HEY, or Q-), ignore this grade, inform the user and prompt again.
+    # 1.3 Prompt the user for the next grade. If the grade is invalid (e.g., E+, +A, G, HEY, or Q-), ignore this grade, inform the user and prompt again.
 
     1.4 Here, the user entered a valid grade. Using a list or a series of if-elif-else statements, translate the grade to points and accumulate the total points and the count so you can calculate an overall average later. Prompt for the next grade.
 
@@ -15,32 +15,55 @@
     Regex Pattern for letter Grade
     https://regex101.com/r/uvsEdU/3
 """
-from functools import reduce
 from re import match
 
 
-def sum_grade_points(points: list):
-    def _sum(x, y): return x + y
-
-    return reduce(_sum, points)
+"""
+This method uses regex to match correctly input letter grades,
+"""
 
 
 def is_valid_letter_grade(letter_grade: str) -> bool:
-    valid_grade_re = r'^[A-D,F][-+]?$'
+    valid_grade_re = r'^[A-D][-+]?$'
     return match(valid_grade_re, letter_grade)
 
 
-def points_for_letter_grade(letter_grade: str) -> float:
-    return {
-        'A+': 4.2, 'A': 4.0, 'A-': 3.9,
-        'B+': 3.7, 'B': 3.2, 'B-': 3.0,
-        'C+': 2.8, 'C': 2.2, 'C-': 2.0,
-        'D+': 1.8, 'D': 1.2, 'F': 0,
-    }[letter_grade]
+"""
+This method will update the counter of the letter grade the user input 
+"""
+
+
+def update_grade_book(grade_book: dict, letter_grade: str) -> dict:
+    grade_book[letter_grade][0] += 1
+    return grade_book
+
+
+""" 
+This dict represents a dict where the keys, represented by letter grades have a tuple value
+The tuple's left side represents how many of that Letter grade the user input
+The tuple's right side represent how many point that letter grade is worth
+
+grade_book: dict(str, list(int, float))
+"""
+grade_book = {
+    'A+': [0, 4.2], 'A': [0, 4.0], 'A-': [0, 3.9],
+    'B+': [0, 3.7], 'B': [0, 3.2], 'B-': [0, 3.0],
+    'C+': [0, 2.8], 'C': [0, 2.2], 'C-': [0, 2.0],
+    'D+': [0, 1.8], 'D': [0, 1.2],
+}
 
 
 def main():
-    pass
+    while True:
+        grade_input = input("Please enter a letter grade: ")
+        if grade_input == '' or grade_input == 'quit':
+            print(grade_book)
+            break
+        else:
+            if is_valid_letter_grade(grade_input):
+                update_grade_book(grade_book, grade_input)
+            else:
+                print("Enter a valid letter grade. F is ignored.")
 
 
 if __name__ == "__main__":
