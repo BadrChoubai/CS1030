34f8aa172d744632e4bd684b2b37a76d0441b491
Badr Choubai <badrchoubai@outlook.com>
2020-02-22 13:45:12 -0700

    cut the fancy shit out, implement actual required game loop

--- Python_Projects/python_project_2/badr_choubai_02_02__01.py
+++ Python_Projects/python_project_2/badr_choubai_02_02__01.py
@@ -18,8 +18,8 @@
 from re import match
 
 
-"""
-This method uses regex to match correctly input letter grades,
+"""is_valid_letter_grade
+This method implements a regex pattern to check that the letter grade input by the user is valid
 """
 
 
@@ -28,42 +28,36 @@ def is_valid_letter_grade(letter_grade: str) -> bool:
     return match(valid_grade_re, letter_grade)
 
 
-"""
-This method will update the counter of the letter grade the user input 
-"""
+scores = {
+    'A+': 4.2, 'A': 4.0, 'A-': 3.9,
+    'B+': 3.7, 'B': 3.2, 'B-': 3.0,
+    'C+': 2.8, 'C': 2.2, 'C-': 2.0,
+    'D+': 1.8, 'D': 1.2,
+}
 
-
-def update_grade_book(grade_book: dict, letter_grade: str) -> dict:
-    grade_book[letter_grade][0] += 1
-    return grade_book
-
-
-""" 
-This dict represents a dict where the keys, represented by letter grades have a tuple value
-The tuple's left side represents how many of that Letter grade the user input
-The tuple's right side represent how many point that letter grade is worth
-
-grade_book: dict(str, list(int, float))
-"""
-grade_book = {
-    'A+': [0, 4.2], 'A': [0, 4.0], 'A-': [0, 3.9],
-    'B+': [0, 3.7], 'B': [0, 3.2], 'B-': [0, 3.0],
-    'C+': [0, 2.8], 'C': [0, 2.2], 'C-': [0, 2.0],
-    'D+': [0, 1.8], 'D': [0, 1.2],
+score_counter = {
+    'A+': 0, 'A': 0, 'A-': 0,
+    'B+': 0, 'B': 0, 'B-': 0,
+    'C+': 0, 'C': 0, 'C-': 0,
+    'D+': 0, 'D': 0,
 }
 
 
 def main():
     while True:
-        grade_input = input("Please enter a letter grade: ")
-        if grade_input == '' or grade_input == 'quit':
-            print(grade_book)
+        grade_input = input("Give me a letter grade: ")
+
+        if grade_input == 'quit':
+            print('Quit')
             break
+        elif grade_input == '':
+            print('no input')
+            continue
         else:
             if is_valid_letter_grade(grade_input):
-                update_grade_book(grade_book, grade_input)
+                score_counter[grade_input] += 1
             else:
-                print("Enter a valid letter grade. F is ignored.")
+                print("Please input a valid letter grade. Fs are ignored")
 
 
 if __name__ == "__main__":
