239c3a088927208c21ca7fa40f02f29c3270b92b
Badr Choubai <badrchoubai@outlook.com>
2020-02-22 17:36:52 -0700

    2.3 2.4 2.5 2.6 2.7

--- Python_Projects/python_project_2/badr_choubai_02_02__02.py
+++ Python_Projects/python_project_2/badr_choubai_02_02__02.py
@@ -3,30 +3,46 @@
 
     # 2.2 Loop N times.
 
-    2.3 Loop until you find three heads or three tails in a row, HHH or TTT, respectively.
+    # 2.3 Loop until you find three heads or three tails in a row, HHH or TTT, respectively.
 
-    2.4 Start flipping coins. Use randint(1,2) to generate a 1 or 2 for heads and tails respectively.
+    # 2.4 Start flipping coins. Use randint(1,2) to generate a 1 or 2 for heads and tails respectively.
+        - I chose to implement 'choice' from the random module
 
-    2.5 Count flips and check for HHH or TTT.
+    # 2.5 Count flips and check for HHH or TTT.
 
-    2.6 When you get HHH or TTT, print the flips in the current simulation and accumulate the number of flips, minimum number of flips to get three in a row and the highest number of flips to get three in a row. The flips should be displayed with Hs and Ts with a space between each flip, like H T T H H H
+    # 2.6 When you get HHH or TTT, print the flips in the current simulation and accumulate the number of flips, minimum number of flips to get three in a row and the highest number of flips to get three in a row. The flips should be displayed with Hs and Ts with a space between each flip, like H T T H H H
         
         choice = choice(('H', 'T'))    
         Something like ''.join(f'{ choice } ') 
 
-    2.7 Go on to the next simulation, if there is one.
+    # 2.7 Go on to the next simulation, if there is one.
 
     2.8 Print the average number of flips, the minimum number of flips and maximum number of flips.
 
     2.9 Resume at step 2.1.
 """
-from random import choice
+
+
+def flip_coin() -> str:
+    from random import choice
+
+    coin = ('H', 'T')
+    return choice(coin)
 
 
 def is_valid_series(series: list) -> bool:
-    triplet = ''.join(series)
-    valid = ('TTT' in triplet or 'HHH' in triplet)
+    series = ''.join(series)
+    valid = ('TTT' in series or 'HHH' in series)
     return valid
+
+
+def print_series_results(flips_total: int, series: list):
+    results_str = ''.join(series)
+    print(f"""
+    Flips Total: { flips_total };
+    Triplet String: { ''.join(series[-3:]) }
+    Results String: { results_str } 
+    """)
 
 
 def main():
@@ -34,10 +50,22 @@ def main():
         "How many coin flipping simulations would you like to run?: ")
     simulations = int(simulations)
 
-    while simulations > 0:
-        print(simulations)
+    flips_total = 0
+    series: list = []
 
-        simulations -= 1
+    while simulations >= 0:
+
+        while len(series) < 3:
+            series.append(flip_coin())
+            flips_total += 1
+
+        if is_valid_series(series):
+            print_series_results(flips_total, series)
+            series.clear()
+            flips_total = 0
+            simulations -= 1
+        else:
+            series.clear()
 
 
 if __name__ == "__main__":
