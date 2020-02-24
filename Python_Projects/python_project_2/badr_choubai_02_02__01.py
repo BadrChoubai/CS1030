431bf1b9298c0338aa6f8a850c36f88a40ab209d
Badr Choubai <badrchoubai@outlook.com>
2020-02-24 12:40:58 -0700

    (Python Project 2): Clean up

--- Python_Projects/python_project_2/badr_choubai_02_02__01.py
+++ Python_Projects/python_project_2/badr_choubai_02_02__01.py
@@ -1,19 +1,8 @@
 """
-    # 1.1 Loop until the user enters ‘quit’ at which time proceed at step 1.6.
- 
-    # 1.2 Do an inner loop until the user enters a blank line.
-
-    # 1.3 Prompt the user for the next grade. If the grade is invalid (e.g., E+, +A, G, HEY, or Q-), ignore this grade, inform the user and prompt again.
-
-    # 1.4 Here, the user entered a valid grade. Using a list or a series of if-elif-else statements, translate the grade to points and accumulate the total points and the count so you can calculate an overall average later. Prompt for the next grade.
-
-    # 1.5 If the user enters a blank line, calculate and print the GPA for the grades just entered. Your program should correctly handle the case where the user enters no grades. In that case, print “No GPA calculated” and resume at step 1.2.
-
-    # 1.6 Calculate and print the overall average of all GPAs that were processed. 
-        - Overall Average = Sum of all points / Number of grades processed
-
-    Regex Pattern for letter Grade
-    https://regex101.com/r/uvsEdU/3
+Name: Badr Choubai
+Professor: David Kramer
+Class: CS 1030
+Project: Python Project Two
 """
 from functools import reduce
 from re import match
@@ -26,8 +15,6 @@ def calculate_points(grade_input_stream: list, scores: dict) -> int:
         points += scores[lg]
 
     return points
-
-
 
 
 def is_valid_letter_grade(letter_grade: str) -> bool:
