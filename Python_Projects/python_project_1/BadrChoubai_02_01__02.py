b6ff93ced2458887688b8ddcef0e35fc2abc26e1
Badr Choubai <badrchoubai@outlook.com>
2020-02-25 21:00:12 -0700

    (Python Project One): Refactor problem two

--- Python_Projects/python_project_1/BadrChoubai_02_01__02.py
+++ Python_Projects/python_project_1/BadrChoubai_02_01__02.py
@@ -6,21 +6,11 @@ Project: Python Project One
 """
 
 
-def get_user_input(prompts: list) -> list:
-    prompt_answers: list = []
-
-    for prompt_message in prompts:
-        answer = input(f"{ prompt_message }: ")
-        prompt_answers.append(int(answer.strip()))
-
-    return prompt_answers
-
-
 def main():
-    prompts: list = ["Enter miles travelled",
-                     "Gallons of fuel used? "]
-
-    miles, gallons = get_user_input(prompts)
+    miles = input("Enter miles travelled: ")
+    gallons = input("Miles travelled?: ")
+    miles = int(miles)
+    gallons = int(gallons)
 
     kilometers = (miles * 1.60934)
     liters = (gallons * 3.7854)
@@ -35,7 +25,7 @@ def main():
     mpg_str = f"{miles_per_gallon:.2f} mpg"
     lpk_str = f"{liters_per_kilometer:.2f} lpk"
 
-    output_str = f"Miles Per Gallon: {mpg_str} \nLiters Per Kilometer: {lpk_str}"
+    output_str = f"Miles Per Gallon -> {mpg_str} \nLiters Per Kilometer -> {lpk_str}"
 
     print(output_str)
 
