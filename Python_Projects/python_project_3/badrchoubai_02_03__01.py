2cff75261b4d7880168dec3533804d9d1414f9b2
Badr Choubai <badrchoubai@outlook.com>
2020-03-10 11:15:08 -0600

    (PP3): Add solution for PP3, Pr 1

--- Python_Projects/python_project_3/badrchoubai_02_03__01.py
+++ Python_Projects/python_project_3/badrchoubai_02_03__01.py
@@ -4,3 +4,14 @@ Professor: David Kramer
 Class: CS 1030
 Project: Python Project Three
 '''
+
+def convert(degrees_celsius: int)-> float:
+    return (1.8) * (degrees_celsius) + 32 
+
+
+print(f'| Celsius | Fahrenheit |')
+print(f'|---------|------------|')
+for i in range(0, 101, 10):
+    celsius, fahrenheit = i, convert(i)
+    print(f'| {celsius} | {fahrenheit:.0f} |')
+
