8da4e184c021ca59b8223a2e4141a7e5469bf1e9
Badr Choubai <badrchoubai@outlook.com>
2020-03-29 02:18:42 -0600

    (PP3) update problem one solution with formatted output

--- Python_Projects/python_project_3/badrchoubai_02_03__01.py
+++ Python_Projects/python_project_3/badrchoubai_02_03__01.py
@@ -5,7 +5,7 @@ Class: CS 1030
 Project: Python Project Three
 '''
 
-def convert(degrees_celsius: int)-> float:
+def convert(degrees_celsius: int) -> float:
     return (1.8) * (degrees_celsius) + 32 
 
 
@@ -13,5 +13,5 @@ print(f'| Celsius | Fahrenheit |')
 print(f'|---------|------------|')
 for i in range(0, 101, 10):
     celsius, fahrenheit = i, convert(i)
-    print(f'| {celsius} | {fahrenheit:.0f} |')
+    print(f'| {celsius}{" "*(7 - len(str(celsius)))} | {fahrenheit:.0f}{" "*(12 - len(str(fahrenheit)))} |')
 
