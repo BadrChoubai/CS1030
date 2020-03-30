f71fecaa219411cf735a364e8a881e5dfa0fc03e
Badr Choubai <badrchoubai@outlook.com>
2020-03-30 01:20:47 -0600

    (PP3): Lint

--- Python_Projects/python_project_3/badrchoubai_02_03__03.py
+++ Python_Projects/python_project_3/badrchoubai_02_03__03.py
@@ -4,6 +4,7 @@ Professor: David Kramer
 Class: CS 1030
 Project: Python Project Three
 '''
+import sys
 
 
 def print_search_results(results: tuple) -> None:
@@ -16,24 +17,29 @@ def print_search_results(results: tuple) -> None:
         province_list[i] = province_list[i].replace('_', ' ').title()
 
     print(
-        f'The Province Code you entered is for a {province_type} area in {" or the ".join(province_list)}')
+        f'The Province Code you entered is for a {province_type} area \
+            in {" or the ".join(province_list)}')
 
 
 def is_valid_province_code(province_code_input: str) -> bool:
     '''is_valid_postal_code
-    This method takes the postal code input and runs a series of checks to see whether
-    or not it is a valid input.
+    This method takes the postal code input and runs a series of checks
+    to see whether or not it is a valid input.
     '''
     # if postal code starts with one of these letters it will be invalid
     invalid_letters = {'D', 'F', 'I', 'O', 'Q', 'U', 'W', 'Z'}
     valid_length = len(province_code_input) == 7
-    valid_format = province_code_input[3] == ' '
+    valid_format = province_code_input[3].isspace()
     valid_letter = province_code_input[0] not in invalid_letters
 
     return valid_length and valid_format and valid_letter
 
 
 def search_provinces(province_code_input: str) -> tuple:
+    '''search_provinces
+    This method takes the province code input and returns a tuple containing
+    the province type and the province that matches the province code
+    '''
     canadian_provinces = {
         'newfoundland': 'A',
         'nova_scotia': 'B',
@@ -65,11 +71,14 @@ def main():
         province_code_input = input("Enter a Canadian province code: ")
 
         if province_code_input == '':
-            break
+            sys.exit()
         elif is_valid_province_code(province_code_input):
             search_results = search_provinces(province_code_input)
             print_search_results(search_results)
             continue
+        else:
+            print(
+                f'Please enter a valid Canadian province code')
 
 
 if __name__ == '__main__':
