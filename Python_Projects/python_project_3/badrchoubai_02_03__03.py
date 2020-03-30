c11db7bf8590b4cd37b38de20bdaab107f79c3d6
Badr Choubai <badrchoubai@outlook.com>
2020-03-29 23:53:51 -0600

    (PP3): Setup unittesting for problem 3

--- Python_Projects/python_project_3/badrchoubai_02_03__03.py
+++ Python_Projects/python_project_3/badrchoubai_02_03__03.py
@@ -11,30 +11,30 @@ def print_search_results(results: tuple) -> None:
     This method takes the results from the search_provinces method and pretty
     prints them.
     '''
-    provinces, province_type = results
-    for i, _ in enumerate(provinces):
-        provinces[i] = provinces[i].replace('_', ' ').title()
+    province_list, province_type = results
+    for i, _ in enumerate(province_list):
+        province_list[i] = province_list[i].replace('_', ' ').title()
 
     print(
-        f'The Province Code you entered is for a { province_type } area in { " or the ".join(provinces) }')
+        f'The Province Code you entered is for a {province_type} area in {" or the ".join(province_list)}')
 
 
-def is_valid_province_code(postal_code_input: str) -> bool:
+def is_valid_province_code(province_code_input: str) -> bool:
     '''is_valid_postal_code
     This method takes the postal code input and runs a series of checks to see whether
     or not it is a valid input.
     '''
     # if postal code starts with one of these letters it will be invalid
     invalid_letters = {'D', 'F', 'I', 'O', 'Q', 'U', 'W', 'Z'}
-    valid_length = len(postal_code_input) == 7
-    valid_format = postal_code_input[3] == ' '
-    valid_letter = postal_code_input[0] not in invalid_letters
+    valid_length = len(province_code_input) == 7
+    valid_format = province_code_input[3] == ' '
+    valid_letter = province_code_input[0] not in invalid_letters
 
     return valid_length and valid_format and valid_letter
 
 
-def search_provinces(postal_code_input: str) -> tuple:
-    canadian_postal_codes = {
+def search_provinces(province_code_input: str) -> tuple:
+    canadian_provinces = {
         'newfoundland': 'A',
         'nova_scotia': 'B',
         'prince_edward_island': 'C',
@@ -49,22 +49,28 @@ def search_provinces(postal_code_input: str) -> tuple:
         'northwest_territories': 'X',
         'yukon': 'Y',
     }
-    province_type = 'Rural' if postal_code_input[1] == '0' else 'Urban'
-    provinces = []
+    province_type = 'Rural' if province_code_input[1] == '0' else 'Urban'
+    province_list = []
 
-    letter = postal_code_input[0]
-    for province, letters in canadian_postal_codes.items():
+    letter = province_code_input[0]
+    for province, letters in canadian_provinces.items():
         if letter in letters:
-            provinces.append(province)
+            province_list.append(province)
 
-    return (provinces, province_type)
+    return (province_list, province_type)
 
 
-while True:
-    province_code_input = input("Enter a Canadian postal code: ")
+def main():
+    while True:
+        province_code_input = input("Enter a Canadian province code: ")
 
-    if province_code_input == '':
-        break
-    elif is_valid_province_code(province_code_input):
-        print_search_results(search_provinces(province_code_input))
-        continue
+        if province_code_input == '':
+            break
+        elif is_valid_province_code(province_code_input):
+            search_results = search_provinces(province_code_input)
+            print_search_results(search_results)
+            continue
+
+
+if __name__ == '__main__':
+    main()
