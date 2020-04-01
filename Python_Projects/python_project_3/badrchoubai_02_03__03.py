11cc9c738b88464d2ad7619018579d2f5e4a9ba7
Badr Choubai <badrchoubai@outlook.com>
2020-04-01 15:32:45 -0600

    (PP3): problem three naming

--- Python_Projects/python_project_3/badrchoubai_02_03__03.py
+++ Python_Projects/python_project_3/badrchoubai_02_03__03.py
@@ -21,21 +21,21 @@ def print_search_results(results: tuple) -> None:
             in {" or the ".join(province_list)}')
 
 
-def is_valid_province_code(province_code_input: str) -> bool:
+def is_valid_postal_code(postal_code_input: str) -> bool:
     '''is_valid_postal_code
     This method takes the postal code input and runs a series of checks
     to see whether or not it is a valid input.
     '''
     # if postal code starts with one of these letters it will be invalid
     invalid_letters = {'D', 'F', 'I', 'O', 'Q', 'U', 'W', 'Z'}
-    valid_length = len(province_code_input) == 7
-    valid_format = province_code_input[3].isspace()
-    valid_letter = province_code_input[0] not in invalid_letters
+    valid_length = len(postal_code_input) == 7
+    valid_format = postal_code_input[3].isspace()
+    valid_letter = postal_code_input[0] not in invalid_letters
 
     return valid_length and valid_format and valid_letter
 
 
-def search_provinces(province_code_input: str) -> tuple:
+def search_provinces(postal_code_input: str) -> tuple:
     '''search_provinces
     This method takes the province code input and returns a tuple containing
     the province type and the province that matches the province code
@@ -55,10 +55,10 @@ def search_provinces(province_code_input: str) -> tuple:
         'northwest_territories': 'X',
         'yukon': 'Y',
     }
-    province_type = 'Rural' if province_code_input[1] == '0' else 'Urban'
+    province_type = 'Rural' if postal_code_input[1] == '0' else 'Urban'
     province_list = []
 
-    letter = province_code_input[0]
+    letter = postal_code_input[0]
     for province, letters in canadian_provinces.items():
         if letter in letters:
             province_list.append(province)
@@ -68,12 +68,12 @@ def search_provinces(province_code_input: str) -> tuple:
 
 def main():
     while True:
-        province_code_input = input("Enter a Canadian province code: ")
+        postal_code_input = input("Enter a Canadian province code: ")
 
-        if province_code_input == '':
+        if postal_code_input == '':
             sys.exit()
-        elif is_valid_province_code(province_code_input):
-            search_results = search_provinces(province_code_input)
+        elif is_valid_postal_code(postal_code_input):
+            search_results = search_provinces(postal_code_input)
             print_search_results(search_results)
             continue
         else:
