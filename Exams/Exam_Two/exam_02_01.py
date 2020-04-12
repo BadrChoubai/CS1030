d75c31122d38a68b50c23e18e540d0aecec6aa8f
Badr Choubai <badrchoubai@outlook.com>
2020-04-12 15:29:37 -0600

    (Exams): minor

--- Exams/Exam_Two/exam_02_01.py
+++ Exams/Exam_Two/exam_02_01.py
@@ -1,17 +1,3 @@
-'''
-1.(10 points)Write a program that prompts repeatedly for decimal numbers. Store each number in a list. Stop asking for numbers when the user enters a 0 which is not stored in the list. Calculate and print two lines of output as follows: 
-
-Line 1The sum of all the numbers: sum goes here 
-Line 2The average of all the numbers: average goes here 
-
-Example: for inputs of 10.5, 9.5, 8, -6.0 and 0, the four numbers are stored in a list and the program generates this output: 
- 
-The sum of all the numbers: 22.0 
-
-The average of all the numbers: 5.5 
-'''
-
-
 def calculate_average(decimal_numbers: [float]) -> float:
     result = 0
     try:
