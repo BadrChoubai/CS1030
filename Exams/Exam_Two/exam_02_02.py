9d85dc4c2508fd9e364970a8c998b4819717c997
Badr Choubai <badrchoubai@outlook.com>
2020-04-12 08:09:25 -0600

    (Exams): update problem 2 solution

--- Exams/Exam_Two/exam_02_02.py
+++ Exams/Exam_Two/exam_02_02.py
@@ -1,13 +1,5 @@
 import random
 
-
-def unique(lottery_numbers: list) -> bool:
-    return sum(set(lottery_numbers)) == sum(lottery_numbers)
-
-
-lottery_numbers = [random.randint(1, 49) for i in range(6)]
-
-while not unique(lottery_numbers):
-    lottery_numbers = [random.randint(1, 49) for i in range(6)]
+lottery_numbers = random.sample(range(1, 49), 6)
 
 print(sorted(lottery_numbers))
