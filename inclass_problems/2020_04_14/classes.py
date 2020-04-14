a3b75ed6dd935f12a12fc5f8e31a82f6d0b9eb26
Badr Choubai <badrchoubai@outlook.com>
2020-04-14 15:20:37 -0600

    (inclass): Add code for Intro to OOP in Python

--- inclass_problems/2020_04_14/classes.py
+++ inclass_problems/2020_04_14/classes.py
@@ -2,6 +2,9 @@
 
 
 class Employee():
+    number_of_employees = 0
+    raise_percentage = 1.04
+
     def __init__(self, first_name: str, last_name: str, position: str, salary: int):
         self.first_name = first_name
         self.last_name = last_name
@@ -9,10 +12,19 @@ class Employee():
         self.salary = salary
         self.email = f'{first_name}{last_name}@company.com'
 
+        # This will help us keep track of how many Employee instances we have created
+        Employee.number_of_employees += 1
+
     def full_name(self):
         print(f'{self.first_name} {self.last_name}')
+
+    def apply_raise(self):
+        self.salary = int(self.salary * self.raise_percentage)
 
 
 employee_1 = Employee("Badr", "Choubai", "Software Engineer", 70_000)
 
 employee_1.full_name()
+print(employee_1.salary)
+employee_1.apply_raise()
+print(employee_1.salary)
