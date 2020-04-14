516fc6bb5a2ab1ae6fbcc665b628316c2bfd474d
Badr Choubai <badrchoubai@outlook.com>
2020-04-14 15:33:56 -0600

    (inclass): Add code for intro to OOP in Python

--- inclass_problems/2020_04_14/classes.py
+++ inclass_problems/2020_04_14/classes.py
@@ -10,21 +10,30 @@ class Employee():
         self.last_name = last_name
         self.position = position
         self.salary = salary
-        self.email = f'{first_name}{last_name}@company.com'
+        self.email = f'{first_name}.{last_name}@company.com'
 
         # This will help us keep track of how many Employee instances we have created
         Employee.number_of_employees += 1
 
     def full_name(self):
-        print(f'{self.first_name} {self.last_name}')
+        return (f'{self.first_name} {self.last_name}')
 
     def apply_raise(self):
         self.salary = int(self.salary * self.raise_percentage)
 
+    @classmethod
+    def set_raise_amount(cls, new_raise_percentage):
+        cls.raise_percentage = new_raise_percentage
+
+    @classmethod
+    def from_string(cls, employee_info_str):
+        first_name, last_name, position, salary = employee_info_str.split(',')
+        return cls(first_name, last_name, position, int(salary))
+
 
 employee_1 = Employee("Badr", "Choubai", "Software Engineer", 70_000)
+print(employee_1.full_name())
 
-employee_1.full_name()
-print(employee_1.salary)
-employee_1.apply_raise()
-print(employee_1.salary)
+employee_info_str = 'John,Doe,Software Engineer,70000'
+new_employee = Employee.from_string(employee_info_str)
+print(new_employee.full_name())
