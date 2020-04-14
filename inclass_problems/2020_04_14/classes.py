bbf4d6167f74ad325164cfc7e56c520448b20827
Badr Choubai <badrchoubai@outlook.com>
2020-04-14 14:38:29 -0600

    (inclass): Add code for Intro to OOP in Python

---
+++ inclass_problems/2020_04_14/classes.py
@@ -0,0 +1,18 @@
+# Python Object-Oriented Programming
+
+
+class Employee():
+    def __init__(self, first_name: str, last_name: str, position: str, salary: int,):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.position = position
+        self.salary = salary
+        self.email = f'{first_name}{last_name}@company.com'
+
+    def full_name(self):
+        print(f'{self.first_name} {self.last_name}')
+
+
+employee_1 = Employee("Badr", "Choubai", "Software Engineer", 70_000)
+
+employee_1.full_name()
