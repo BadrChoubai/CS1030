7780d08bfbf98bc840f4799e47fb7628af855a55
Badr Choubai <badrchoubai@outlook.com>
2020-04-16 03:42:18 -0600

    minor

--- inclass_problems/2020_04_14/classes.py
+++ inclass_problems/2020_04_14/classes.py
@@ -2,10 +2,12 @@
 
 
 class Employee():
+    # Class Variables
     number_of_employees = 0
     raise_percentage = 1.04
 
     def __init__(self, first_name: str, last_name: str, position: str, salary: int):
+        # Instance Variables
         self.first_name = first_name
         self.last_name = last_name
         self.position = position
@@ -15,6 +17,7 @@ class Employee():
         # This will help us keep track of how many Employee instances we have created
         Employee.number_of_employees += 1
 
+    # Instance Methods
     def full_name(self):
         return (f'{self.first_name} {self.last_name}')
 
