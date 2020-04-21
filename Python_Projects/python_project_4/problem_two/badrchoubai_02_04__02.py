0decc9881b171fbc03c6b427378383e2824ab8d4
Badr Choubai <badrchoubai@outlook.com>
2020-04-21 15:12:20 -0600

    (PP4): Update solution to problem two

--- Python_Projects/python_project_4/problem_two/badrchoubai_02_04__02.py
+++ Python_Projects/python_project_4/problem_two/badrchoubai_02_04__02.py
@@ -1,27 +1,31 @@
 from string import ascii_uppercase
 
 
-def update_letter_counts(sentence: list, char_occurences: dict) -> dict:
-    for char in sentence:
+def update_char_occurences(characters: list, char_occurences: dict) -> dict:
+    for char in characters:
         char_occurences[char] += 1
 
     return char_occurences
 
 
 def main():
+    # create dictionary with characters A-Z; set values to 0;
     char_occurences = dict(
         zip(ascii_uppercase, [0 for i in range(26)]))
     input_file = 'test sentences.txt'
 
+    # Open the input file
     with open(input_file, 'r') as sentence_file:
+        # parse and clean up each line in file
         for line in sentence_file.readlines():
             if not line.isspace():
                 line = line.replace('\n', '')
-                line = line.upper()
-                update_letter_counts(
-                    [char for char in line if char.isalpha()], char_occurences)
+                print(line)
 
-    # Work on output display and file output
+                update_char_occurences(
+                    [char.upper() for char in line if char.isalpha()], char_occurences)
+
+    # Work on final output to console and to file
 
 
 if __name__ == "__main__":
