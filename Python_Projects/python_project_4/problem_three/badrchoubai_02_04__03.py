4924590e426ea77feb2f91ca55f250d2d1bd93ae
Badr Choubai <badrchoubai@outlook.com>
2020-04-25 00:43:16 -0600

    (PP4): Final solution for problem three

--- Python_Projects/python_project_4/problem_three/badrchoubai_02_04__03.py
+++ Python_Projects/python_project_4/problem_three/badrchoubai_02_04__03.py
@@ -1,31 +1,37 @@
-def parse_input_file(file_path: str) -> list:
-    output = []
+from contextlib import ExitStack
+# ExitStack Documentation :: https://docs.python.org/3/library/contextlib.html#contextlib.ExitStack
+# ExitStack Blog Post :: https://www.rath.org/on-the-beauty-of-pythons-exitstack.html
 
-    try:
-        with open(file_path) as file_list:
-            for line in file_list.readlines():
-                if '\n' in line:
-                    line = line.replace('\n', '')
 
-                output.append(line)
-
-    except FileNotFoundError:
-        print('Input file not found: ', file_path)
-
-    finally:
-        return output
+def open_files(file_list, mode):
+    '''Utility function that implements `contextstack.ExitStack`
+    to handle the opening of multiple files
+    '''
+    flist = []
+    with ExitStack() as stack:
+        for file in file_list:
+            flist.append(stack.enter_context(open(file, mode)))
+        stack.pop_all()
+        return flist
 
 
 def main():
     input_file = '1030 Project 04 03 Files.txt'
     output_file = 'BadrChoubai 02 04 03 Output.txt'
-    files_to_combine = parse_input_file(input_file)
+    files_to_combine = []
 
-    with open(output_file, 'w') as output_file:
-        for file in files_to_combine:
-            with open(file + '.txt', 'r') as content_file:
-                output_file.write(content_file.read() + '\n')
+    with open(input_file, mode='r') as file_list:
+        with open(output_file, mode='w') as output_file:
+            for line in file_list.readlines():
+                files_to_combine.append(line.replace('\n', '.txt'))
+
+            for file in open_files(files_to_combine, mode='r'):
+                output_file.write(file.read() + '\n')
+                file.close()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except FileNotFoundError as error:
+        print(error)
