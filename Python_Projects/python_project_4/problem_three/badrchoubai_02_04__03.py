89e0c152d348a384897cfc542aef334a18092a23
Badr Choubai <badrchoubai@outlook.com>
2020-04-29 06:22:36 -0600

    (PP4): update problem 3 solution

--- Python_Projects/python_project_4/problem_three/badrchoubai_02_04__03.py
+++ Python_Projects/python_project_4/problem_three/badrchoubai_02_04__03.py
@@ -3,31 +3,41 @@ from contextlib import ExitStack
 # ExitStack Blog Post :: https://www.rath.org/on-the-beauty-of-pythons-exitstack.html
 
 
-def open_files(file_list, mode):
+def combine_files(files_to_combine: list, output_file: str):
+    '''combine_files
+    This function takes the list of files we'd like to write to our output file
+    '''
+    output_file = open(output_file, mode='w')
+
+    for file in files_to_combine:
+        output_file.write(file.read() + '\n')
+        file.close()
+
+    output_file.close()
+
+
+def open_files(file_list: list, mode: str) -> list:
     '''Utility function that implements `contextstack.ExitStack`
     to handle the opening of multiple files
     '''
-    flist = []
     with ExitStack() as stack:
-        for file in file_list:
-            flist.append(stack.enter_context(open(file, mode)))
+        for i, file in enumerate(file_list):
+            file_list[i] = stack.enter_context(open(file, mode))
         stack.pop_all()
-        return flist
+        return file_list
 
 
 def main():
     input_file = '1030 Project 04 03 Files.txt'
     output_file = 'BadrChoubai 02 04 03 Output.txt'
-    files_to_combine = []
+    file_list = []
 
-    with open(input_file, mode='r') as file_list:
-        with open(output_file, mode='w') as output_file:
-            for line in file_list.readlines():
-                files_to_combine.append(line.replace('\n', '.txt'))
+    input_file = open(input_file, mode='r')
 
-            for file in open_files(files_to_combine, mode='r'):
-                output_file.write(file.read() + '\n')
-                file.close()
+    for line in input_file.readlines():
+        file_list.append(line.replace('\n', '.txt'))
+
+    combine_files(open_files(file_list, mode='r'), output_file)
 
 
 if __name__ == "__main__":
