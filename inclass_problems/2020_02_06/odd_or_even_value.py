34f8a6679ff9860936a5d95ae3c9b44a88425e8e
Badr Choubai <badrchoubai@outlook.com>
2020-04-30 21:01:33 -0600

    ?

--- inclass_problems/2020_02_06/odd_or_even_value.py
+++ inclass_problems/2020_02_06/odd_or_even_value.py
@@ -1,8 +1,7 @@
-def odd_or_even_value(value: int) -> str:
-    return 'Even' if value % 2 == 0 else 'Odd'
+def odd_or_even(value: int) -> str:
+    return ["odd", "even"][value % 2 == 0]
 
 
 if __name__ == "__main__":
     print(odd_or_even_value(2))
     print(odd_or_even_value(3))
-
