4f2363bda69593c93561b355a48e14fbdde1fdb7
Badr Choubai <badrchoubai@outlook.com>
2020-05-07 14:12:54 -0600

    (Exam Three): problem one

---
+++ Exams/Exam_Three/exam_03_01.py
@@ -0,0 +1,15 @@
+months = [
+    (31, ['january', 'march', 'may', 'july', 'august', 'october', 'december']),
+    (30, ['april', 'june', 'september', 'november']),
+    ('28 or 29 days', ['February'])
+]
+
+month = input('Enter a month of the year: ')
+
+month = month.lower()
+days_in_month = None
+for days, months in months:
+    if month in months:
+        days_in_month = days
+
+print(f'There are {days_in_month} days in {month}')
