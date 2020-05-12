7df673a44348cc442d4615018676baac77919ce1
Badr Choubai <badrchoubai@outlook.com>
2020-05-12 03:25:57 -0600

    implement solution for Exam Two, Problem Three

--- Exams/Exam_Two/Java/ExamTwoP3.java
+++ Exams/Exam_Two/Java/ExamTwoP3.java
@@ -1,5 +1,26 @@
 package Exams.Exam_Two.Java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
 public class ExamTwoP3 {
-	public static void main(String[] args) {}
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("ZipAndCity.txt");
+		System.out.printf("File Exists: %s\n", file.exists());
+		Scanner scan = new Scanner(file);
+		Map<String, String> zipCodes = new HashMap<>();
+
+		while (scan.hasNextLine()) {
+			String line = scan.nextLine();
+			int delineatorChar = line.indexOf(',');
+			zipCodes.put((line.substring(0, delineatorChar - 1)),
+					line.substring(delineatorChar + 1, line.length() - 1));
+		}
+
+		scan.close();
+		System.out.println(zipCodes);
+	}
 }
