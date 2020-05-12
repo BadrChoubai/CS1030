7fce1cdd16a221c0b9f61bf7de59aaccd7bc42e3
Badr Choubai <badrchoubai@outlook.com>
2020-05-12 02:57:37 -0600

    implement solution for Exam Two, Problem Two

--- Exams/Exam_Two/Java/ExamTwoP2.java
+++ Exams/Exam_Two/Java/ExamTwoP2.java
@@ -1,5 +1,22 @@
 package Exams.Exam_Two.Java;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ExamTwoP2 {
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		List<Integer> lotto = new ArrayList<Integer>(6);
+		Random numberGenerator = new Random();
+
+		for (int i = 0; i < 6; i++) {
+			int randInt = numberGenerator.nextInt(49);
+			while (lotto.contains(randInt)) {
+				randInt = numberGenerator.nextInt(49 - 1 + 1) + 1; // https://stackoverflow.com/questions/20389890/generating-a-random-number-between-1-and-10-java
+			}
+
+			lotto.add(randInt);
+		}
+		System.out.println(lotto);
+	}
 }
