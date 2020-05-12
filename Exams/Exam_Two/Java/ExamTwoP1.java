e9baa611593a91128f99fe239c77bf092510dc6c
Badr Choubai <badrchoubai@outlook.com>
2020-05-12 02:31:21 -0600

    implement solution for Exam Two, Problem One

--- Exams/Exam_Two/Java/ExamTwoP1.java
+++ Exams/Exam_Two/Java/ExamTwoP1.java
@@ -1,5 +1,32 @@
 package Exams.Exam_Two.Java;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExamTwoP1 {
-	public static void main(String[] args) {}
+	private static Scanner inputScanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		List<Double> numbers = new ArrayList<Double>();
+		boolean using = true;
+		double sum = 0;
+		double average = 0;
+
+		do {
+			System.out.println("Give me a number: ");
+			double numberInput = inputScanner.nextDouble();
+
+			if (numberInput != 0.0) {
+				numbers.add(numberInput);
+			} else {
+				sum = numbers.stream().reduce(0.0, Double::sum); // Method Reference
+				average = sum / numbers.size();
+				using = false;
+			}
+		} while (using);
+
+		System.out.printf("Sum of all numbers: %.2f\n", sum);
+		System.out.printf("Calculated average: %.2f\n", average);
+	}
 }
