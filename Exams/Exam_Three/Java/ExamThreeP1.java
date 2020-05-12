7ede508b4a08abf6e992de4254e59fe634ebf5bb
Badr Choubai <badrchoubai@outlook.com>
2020-05-12 03:31:03 -0600

    (ops): stub out java code

---
+++ Exams/Exam_Three/Java/ExamThreeP1.java
@@ -0,0 +1,5 @@
+package Exams.Exam_Three.Java;
+
+public class ExamThreeP1 {
+    public static void main(String[] args) {} 
+}
---
+++ Exams/Exam_Three/Java/ExamThreeP2.java
@@ -0,0 +1,5 @@
+package Exams.Exam_Three.Java;
+
+public class ExamThreeP2 {
+    public static void main(String[] args) {} 
+}
