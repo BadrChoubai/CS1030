fcb77bee91ff421efe0ac3ffceb2a27a650e356a
Badr Choubai <badrchoubai@outlook.com>
2020-05-12 00:34:49 -0600

    (refactor): refactor python solution to Ex1P3

--- Exams/Exam_One/Python/exam_01_03.py
+++ Exams/Exam_One/Python/exam_01_03.py
@@ -1,11 +1,9 @@
 def make_change(cents: int) -> dict:
-    coins = (('quarter', 25), ('dime', 10), ('nickel', 5), ('penny', 1))
     counter = {'quarter': 0, 'dime': 0, 'nickel': 0, 'penny': 0}
 
-    for coin, value in coins:
-        while cents - value >= 0: 
+    for coin, value in zip(counter.keys(), [25, 10, 5, 1]):
+        while cents - value >= 0:
             counter[coin] += 1
-            cents -= value 
+            cents -= value
 
     return counter
-
