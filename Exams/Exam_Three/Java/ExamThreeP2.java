b92e44c9cd4a3f2f0fd4f0a0c9cff45d37c72e85
Badr Choubai <badrchoubai@outlook.com>
2020-05-15 16:30:08 -0600

    partial solution, exam three problem two

--- Exams/Exam_Three/Java/ExamThreeP2.java
+++ Exams/Exam_Three/Java/ExamThreeP2.java
@@ -1,5 +1,32 @@
 package Exams.Exam_Three.Java;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExamThreeP2 {
-    public static void main(String[] args) {} 
+    private static Scanner inputScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        var items = new ArrayList<String>();
+        var item = "";
+        do {
+            System.out.println("Input a Word: ");
+            item = inputScanner.nextLine();
+
+            if (item.isEmpty()) {
+                break;
+            }
+
+            items.add(item);
+
+            if (items.size() == 1) {
+                System.out.println(items.get(0));
+            } else {
+                List<String> segmentedList = items.subList(0, items.size() - 1);
+                System.out.printf("%s and %s\n", segmentedList, items.get(items.size() - 1));
+            }
+
+        } while(!item.isEmpty());
+        inputScanner.close();
+    } 
 }
