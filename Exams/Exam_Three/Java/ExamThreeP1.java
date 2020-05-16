4adae9816ad0db4ec87cd767e349ffbd18bd7583
Badr Choubai <badrchoubai@outlook.com>
2020-05-15 19:25:23 -0600

    Add solution for exam three problem one

--- Exams/Exam_Three/Java/ExamThreeP1.java
+++ Exams/Exam_Three/Java/ExamThreeP1.java
@@ -1,5 +1,19 @@
 package Exams.Exam_Three.Java;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ExamThreeP1 {
-    public static void main(String[] args) {} 
+    private static Scanner inputScanner = new Scanner(System.in);
+    private static String[] months = {"january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"};
+    private static String[] days = {"31", "28 or 29 days", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"};
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a month of the year:");
+        var monthInput = inputScanner.nextLine();
+        var searchIndex = Arrays.asList(months).indexOf(monthInput);
+
+        System.out.printf("There are %s days in %s", days[searchIndex], monthInput);
+    }
 }
