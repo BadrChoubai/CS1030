e1035f381ee0a8777bab2699d9e5037a86ef06e9
Badr Choubai <badrchoubai@outlook.com>
2020-05-18 19:09:38 -0600

    (Project One) Problem two

--- Projects/Project_One/Java/ProjectOne02.java
+++ Projects/Project_One/Java/ProjectOne02.java
@@ -1,5 +1,32 @@
 package Projects.Project_One.Java;
 
+import java.util.Scanner;
+
 class ProjectOne02 {
-    public static void main(String[] args) {}
+    private static Scanner inputScanner = new Scanner(System.in);
+
+    private static String getUserInput(String prompt) {
+        System.out.printf("%s:\n> ", prompt);
+        var input = inputScanner.nextLine();
+        return input;
+    }
+
+    public static void main(String[] args) {
+        String milesInput = getUserInput("Miles travelled");
+        String gallonsInput = getUserInput("Gallons of gas used");
+
+        Integer miles = Integer.parseInt(milesInput);
+        Integer gallons = Integer.parseInt(gallonsInput);
+
+        var kilometers = (miles * 1.60934);
+        var liters = (gallons * 3.7854);
+
+        var milesPerGallon = (miles / gallons);
+        var kilometersPerMile = (kilometers / miles);
+        var gallonsPerLiter = (gallons / liters);
+        var litersPerKilometer = 1 / (milesPerGallon * kilometersPerMile * gallonsPerLiter);
+
+        System.out.printf("Miles per gallon: %d\nKilometers per liter: %.2f", milesPerGallon, litersPerKilometer);
+
+    }
 }
