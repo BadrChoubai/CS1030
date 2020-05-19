2cc8578bb67d615811de71e59a26b4dfa52648eb
Badr Choubai <badrchoubai@outlook.com>
2020-05-18 18:58:23 -0600

    (Project One) Project One

--- Projects/Project_One/Java/ProjectOne01.java
+++ Projects/Project_One/Java/ProjectOne01.java
@@ -1,5 +1,28 @@
 package Projects.Project_One.Java;
 
+import java.util.Scanner;
+
 class ProjectOne01 {
-    public static void main(String[] args) {}
+    private static Scanner inputScanner = new Scanner(System.in);
+
+    private static String getUserInput(String prompt) {
+        System.out.printf("%s:\n> ", prompt);
+        var input = inputScanner.nextLine();
+        return input;
+    }
+
+    public static void main(String[] args) {
+        String feetInput = getUserInput("Feet");
+        String inchesInput = getUserInput("Inches");
+        var heightString = String.format("%s'%s\"", feetInput, inchesInput);
+
+        var feet = Integer.parseInt(feetInput);
+        var inches = Integer.parseInt(inchesInput);
+
+        var total = (feet * 12) + inches;
+        var centimeters = total * 2.54;
+        var meters = centimeters / 100;
+
+        System.out.printf("Original height input %s, to meters: %.2fm", heightString, meters);
+    }
 }
