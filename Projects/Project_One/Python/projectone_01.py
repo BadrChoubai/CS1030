7a97196b91bd3bb3066fa0c45c57889e517095ec
Badr Choubai <badrchoubai@outlook.com>
2020-05-20 01:05:00 -0600

    (Project One): refactor Python solution to problem one

--- Projects/Project_One/Python/projectone_01.py
+++ Projects/Project_One/Python/projectone_01.py
@@ -6,38 +6,29 @@ Project: Python Project One
 """
 
 
-def feet_to_inches(feet: int) -> int:
-    return (feet * 12)
-
-
-def centimeters_to_meters(centimeters: float) -> float:
-    return (centimeters / 100)
-
-
 def calculate_meters(inches: int) -> float:
+    inches_to_centimeters = (lambda inches: inches * 2.54)
+    centimeters_to_meters = (lambda centimeters: centimeters / 100)
     return (centimeters_to_meters(inches_to_centimeters(inches)))
 
 
-def inches_to_centimeters(inches: int) -> float:
-    return (inches * 2.54)
-
-
 def main():
+    height_input_feet = input('Feet: ')
+    height_input_inches = input('Inches: ')
+    height_str = f"{height_input_feet}'{height_input_inches}\""
+    output_str = ""
 
-    height = input("Input you height as feet'inches\": ")
-    height = height.replace('\'', ' ').replace('"', '')
+    feet = int(height_input_feet)
+    inches = int(height_input_inches)
+    inches_total = (feet * 12) + inches
 
-    (feet, inches) = height.split(' ')
-    feet = int(feet)
-    inches = int(inches)
+    if inches_total <= 95:
+        meters = calculate_meters(inches_total)
+        output_str = f"Original Height Input {height_str} -> Converted to meters {meters:.2f}m"
+    else:
+        output_str = "Wow, you're so tall!"
 
-    inches_total = feet_to_inches(feet) + inches
-    meters = calculate_meters(inches_total)
-
-    height_str = f"{feet}'-{inches}\""
-    output_str = f"Original Height Input {height_str} -> Converted to meters {meters:.2f}m"
-
-    print(output_str) if inches_total <= 95 else print("Wow, you're so tall!")
+    print(output_str)
 
 
 if __name__ == "__main__":
